4f063a26b3c0111cf5059805da416c860b614fd0
Febby <gunawan.febby@gmail.com>
2014-05-23 14:42:58 +0700

    updated js and added audio file

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
-$(document).ready(function() {
+function playHadouken () {
+	  $('#hadouken-sound')[0].volume = 0.5;
+	  $('#hadouken-sound')[0].load();
+	  $('#hadouken-sound')[0].play();
+	}
 
+var poseMusic = false;
+function playPose () {
+  poseMusic = !poseMusic;
+  if (poseMusic) {
+    $('#pose')[0].pause();
+    $('#pose')[0].play();
+  }
+}
+$(document).ready(function() {
 	//when user hover over
 	$('.ryu').mouseenter(function() {
 		/* Act on the event */
@@ -8,18 +21,25 @@ $(document).ready(function() {
 	})
 	//after user leave ryu
 	.mouseleave(function() {
-
     $('.ryu-ready').hide();
 	$('.ryu-still').show();
   })
 
-	.mousedown(function() {
-		//play hadouke sound
+	.mousedown(function() {	
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
-		
+		//play hadouken sound
+		playHadouken();
+		$('.hadouken').finish().show()
 		// show hadouken and animate it to the right of the screen
+			.animate(
+				  {'left': '800px'},
+				  500,
+				  function() {
+				    $(this).hide();
+				    $(this).css('left', '520px');
+				  }
+			);
 	})
 
 	.mouseup(function() {
@@ -27,5 +47,23 @@ $(document).ready(function() {
   		$('.ryu-ready').show();
 	});
 
+	$(document).keydown(function(event) {
+		if (event.keyCode == 88){
+			playPose();
+			$('.ryu-still').hide();
+			 $('.ryu-cool').show();
+			}
+
+	}).keyup(function(event) {
+		/* Act on the event */
+		if (event.keyCode == 88){
+			$('#pose')[0].pause();
+      		$('#pose')[0].load();
+			$('.ryu-cool').hide();
+			$('.ryu-still').show();
+
+		}
+	});	
+		
 
 });
