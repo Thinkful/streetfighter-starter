155d62bedf6d8d091f2903b4b13f2b5ac9f79372
Daniel Chaboya <daniel.chavolla@gmail.com>
2014-07-11 14:55:57 -0700

    commit2
    add drop down for play instructions

--- js/app.js
+++ js/app.js
@@ -1,31 +1,41 @@
-$(document).ready( function() {
-	$(".ryu").mouseenter(function() {
-		$('.ryu-still').hide();
-		$('.ryu-ready').show();
-	})
-	.mouseleave(function(){
-		$('.ryu-ready').hide();
-		$('.ryu-still').show();
-	})
-
-	.mousedown(function() {
-		console.log('mousedown');
-		$('.ryu-ready').hide();
-		$('.ryu-throwing').show();
-		$('.hadouken').show();
-	})
-
-	.mouseup(function() {
-		console.log('mouseup');
-		$('.ryu-throwing').hide();
-		$('.ryu-ready').show();
-	});
-
-	$(".instruct").on("mouseenter", "img", function() {
+$(document).ready(function() {
+  $('.ryu').mouseenter(function() {
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+  })
+  .mouseleave(function() {
+    $('.ryu-ready').hide();
+    $('.ryu-still').show();
+  })
+  .mousedown(function() {
+    playHadouken();    
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken').finish().show()
+    .animate(
+      {'left': '300px'},
+      500,
+      function() {
+        $(this).hide();
+        $(this).css('left', '-212px');
+      }
+    );
+  })
+  .mouseup(function() {
+    $('.ryu-throwing').hide();
+    $('.ryu-ready').show();
+  });
+  $(".instruct").on("mouseenter", "img", function() {
 		$('ul').slideDown('slow');	
 	})
 
 	.mouseleave(function() {
 		$('ul').slideUp('slow');
-	});	
+	});
 });
+ 
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
