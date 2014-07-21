3ff25dd37e4da8ccbb957cf060a4c7bcb9c505af
GarryWest <garrywest@msn.com>
2014-07-20 22:26:41 -0500

    Lesson 7

--- js/app.js
+++ js/app.js
@@ -2,9 +2,32 @@ $(document).ready(function(){
 	$(".ryu").on("mouseenter",function(){
 		$(".ryu-still").hide();
 		$(".ryu-ready").show();
-	});
-	$(".ryu").on("mouseleave",function(){
+	})
+	.mouseleave(function() {
 		$(".ryu-still").show();
 		$(".ryu-ready").hide();
+	})
+	.mousedown(function() {
+    	playHadouken();
+    	$(".ryu-ready").hide();
+    	$(".ryu-throwing").show();
+    	$('.hadouken').finish().show()
+  .animate(
+    {'left': '300px'},
+    500,
+    function() {
+      $(this).hide();
+      $(this).css('left', '-212px');
+    }
+  );
+  	})
+  	.mouseup(function() {
+  		$(".ryu-throwing").hide();
+  		$(".ryu-ready").show();
 	});
-})
+});
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
