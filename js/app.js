0d29f7173660ac0c32ee177df849e240371fab3a
b-bright <albright.brandi@gmail.com>
2015-07-12 11:06:02 -0700

    Animate Code B Logo Instructions

--- js/app.js
+++ js/app.js
@@ -1,4 +1,28 @@
 $(document).ready(function(){
+	doIntro();
+	playGame();
+});
+
+//HADOKEN SOUND
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
+
+//COOL SOUND FUNCTION
+ var coolSound = false;
+function playCool () {
+  coolSound = !coolSound;
+  if (coolSound) {
+    $('#theme-song')[0].pause();
+    // $('#cool')[0].load()
+    $('#cool')[0].play();
+  }
+}
+
+//MOUSE MOVEMENT ANIMATION	
+function playGame() {
 $('.ryu').mouseenter(function(){
 
 	//alert("mouse entereed .ryu div");
@@ -30,12 +54,42 @@ $('.ryu').mouseenter(function(){
 	//console.log("mouse up fired");
 	$('.ryu-throwing').hide();
 	$('.ryu-ready').show();
- });
-
 });
 
-function playHadouken () {
-  $('#hadouken-sound')[0].volume = 0.5;
-  $('#hadouken-sound')[0].load();
-  $('#hadouken-sound')[0].play();
+//KEY UP KEY DOWN MOVEMENTS
+$(document).keydown(function(e) {
+      if (e.keyCode == 88) {
+      $('.ryu-action').hide();
+      $('.ryu-cool').show(); 
+  }
+  })
+
+ .keyup(function(e) {
+if (e.keyCode == 88) {      
+	  $('#cool')[0].pause();
+      $('#cool')[0].load();
+      $('.ryu-cool').hide();
+      $('.ryu-still').show();
+    }
+  });
+}
+
+
+//INTRO GRAPHICS
+ function doIntro() {
+  $('#theme-song')[0].volume = 0.3;
+  $('#theme-song')[0].play();
+  $('.sf-logo').fadeIn(3500, function() {
+    $(this).fadeOut(1000, function() {
+      $('.brought-by').fadeIn(1500, function() {
+        $(this).fadeOut(1000, function() {
+          $('.jquery-logo').fadeIn(1500, function() {
+            $(this).fadeOut(1500, function() {
+              $('.how-to').fadeIn(1000);
+            });
+          })
+        })
+      })
+    })
+  })
 }
