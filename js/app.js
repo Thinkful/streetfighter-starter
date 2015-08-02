eb32fcd207e15b6623e93e2d3ccb6dc70ca38386
David Gonen <david@curiousminds.com>
2015-08-02 09:51:08 -0700

    unchained click event handler, now click makees image change to hadouken pose.  now need to add handling what happens after click is released

--- js/app.js
+++ js/app.js
@@ -2,8 +2,9 @@ $(document).ready(function() {
   $('.fighter').hover(function() {
       $( this ).css('background-image', 'url("/images/ryu-ready-position.gif")')},
       function() {
-          $( this ).css('background-image', 'url("/images/ryu-standing-still.png")')}).click(function() {
-            $( this ).css('background-image', 'url("/images/ryu-throwing-hadouken.png"'))});
+          $( this ).css('background-image', 'url("/images/ryu-standing-still.png")')});
+  $('.fighter').click(function() {
+            $( this ).css('background-image', 'url("/images/ryu-throwing-hadouken.png")')});
 
 
 
