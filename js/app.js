cf39a5c56fabbe95aa268cfd59b86ee73777f541
David Gonen <david@curiousminds.com>
2015-08-02 15:13:18 -0700

    added hadouken sound and made it play

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
   ).mousedown(function() {
     $( this ).css('background-image', 'url("/images/ryu-throwing-hadouken.png")');
     $('.fireball img').css('display', 'inline');
+    playHadouken();
     $('.fireball img').animate({ "left": "+=700px" }, 500, function() {
       $( this ).css('display', 'none').css('left', '-=700px');
     });
@@ -16,4 +17,20 @@ $(document).ready(function() {
     $( this ).css('background-image', 'url("/images/ryu-standing-still.png")');
   });
 
+
+  $(document).keydown(function() {
+    if( event.which ==  88 || event.which == 120) {
+      $('.fighter').css('background-image', 'url("/images/ryu-cool.gif")');
+    }
+  }).keyup(function() {
+    $('.fighter').css('background-image', 'url("/images/ryu-standing-still.png")');
+  });
+
+
 });
+
+function playHadouken() {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+};
