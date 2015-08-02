57053f2890e83a5439806bbc8c9df14009d2c5b9
David Gonen <david@curiousminds.com>
2015-08-02 09:45:46 -0700

    have the hover in hover out image change working.  Trying to chain a click() event but theres a syntax mistake with ) or } somewhere.  going to unchain the click() and see if I can get the syntax working that way.

---
+++ js/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $('.fighter').hover(function() {
+      $( this ).css('background-image', 'url("/images/ryu-ready-position.gif")')},
+      function() {
+          $( this ).css('background-image', 'url("/images/ryu-standing-still.png")')}).click(function() {
+            $( this ).css('background-image', 'url("/images/ryu-throwing-hadouken.png"'))});
+
+
+
+
+
+
+});
