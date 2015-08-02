6cfa5fd2ddfd853e73b98d7e9bfec09eb27ad410
David Gonen <david@curiousminds.com>
2015-08-02 15:35:14 -0700

    css file references5

--- js/app.js
+++ js/app.js
@@ -4,26 +4,26 @@ $(document).ready(function() {
       $( this ).css('background-image', 'url("../images/ryu-ready-position.gif")');
     },
     function() {
-      $( this ).css('background-image', 'url("/images/ryu-standing-still.png")');
+      $( this ).css('background-image', 'url("../images/ryu-standing-still.png")');
     }
   ).mousedown(function() {
-    $( this ).css('background-image', 'url("/images/ryu-throwing-hadouken.png")');
+    $( this ).css('background-image', 'url("../images/ryu-throwing-hadouken.png")');
     $('.fireball img').css('display', 'inline');
     playHadouken();
     $('.fireball img').animate({ "left": "+=700px" }, 500, function() {
       $( this ).css('display', 'none').css('left', '-=700px');
     });
   }).mouseup(function() {
-    $( this ).css('background-image', 'url("/images/ryu-standing-still.png")');
+    $( this ).css('background-image', 'url("../images/ryu-standing-still.png")');
   });
 
 
   $(document).keydown(function() {
     if( event.which ==  88 || event.which == 120) {
-      $('.fighter').css('background-image', 'url("/images/ryu-cool.gif")');
+      $('.fighter').css('background-image', 'url("../images/ryu-cool.gif")');
     }
   }).keyup(function() {
-    $('.fighter').css('background-image', 'url("/images/ryu-standing-still.png")');
+    $('.fighter').css('background-image', 'url("../images/ryu-standing-still.png")');
   });
 
 
