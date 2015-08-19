caf39b6161218740b76345273b267f4f8cce64be
Kay Patel <kpat108@gmail.com>
2015-08-19 16:51:57 -0700

    Added animation and sound

--- js/app.js
+++ js/app.js
@@ -8,18 +8,46 @@ $(document).ready(function(){
 		$('.ryu-still').show();
 	})
 	.mousedown(function(){
-		console.log('mousedown');
+		// play hadouken sound
+		playHadouken ();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
-		// play hadouken sound
-    	// show hadouken and animate it to the right of the screen
+
+		// show hadouken and animate it to the right of the screen
+		//.finish() completes all currently running animations on an element.
+		$('.hadouken').finish().show().animate(
+			{'left': '1020px'},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '520px')
+			}
+		);
 	})
 	.mouseup(function(){
-		console.log('mouseup');
 		$('.ryu-throwing').hide();
-		$('.ryu-ready').show();
+		$('.ryu-still').show();
 		// ryu goes back to his ready position
 		// hadouken needs to show for animation
-	});
+		});
+
+	$(document).keydown(function(x){
+		if(x.keyCode == 88){
+			$('.ryu-ready').hide();
+			$('.ryu-cool').show();
+		}
+	})
+	.keyup(function(x){
+		if(x.keyCode == 88){
+			$('.ryu-cool').hide();
+			$('.ryu-still').show();
+		}
+	})
 });
+
+ //Load sound for ryu-throw
+  function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
