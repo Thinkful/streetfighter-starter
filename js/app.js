6a1225bc600ff2da099cb37b1014dbfcadefe1f1
Jason Duff <2jduff@gmail.com>
2015-11-08 21:05:58 -0500

    motion

--- js/app.js
+++ js/app.js
@@ -1,8 +1,33 @@
 $(document).ready(function() {
+	function playHadouken () {
+  	$('#hadouken-sound')[0].volume = 0.5;
+  	$('#hadouken-sound')[0].load();
+  	$('#hadouken-sound')[0].play();
+}
 	$('.ryu').mouseenter(function() {
 		//alert('mouse entered .ryu div');//
-		$('.ru-still').hide();
-		$('.ru-ready').show();
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();
 	})
+	.mouseleave(function() {
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
 
+	})
+	.mousedown(function() {
+		playHadouken ();
+  		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').finish().show().animate(
+			{'left':'1024'},
+			500,
+			function(){
+				$(this).hide();
+				$(this).css('left','524px');
+			});
+	})
+	.mouseup(function() {
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
+	});
 });
